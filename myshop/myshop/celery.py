48eb157d4efe1256644f27c75039101b05b89df0
alpha <sultonovmuhibulloh612@gmail.com>
2026-02-28 14:37:32 +0300

    завершенный проект

--- myshop/myshop/celery.py
+++ myshop/myshop/celery.py
@@ -1,14 +1,11 @@
 import os
 from celery import Celery
 
-
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "myshop.settings.production"
+    "myshop.settings.base"  # ← дефолт для локальной разработки
 )
 
 app = Celery("myshop")
-
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
 app.autodiscover_tasks()
--- myshop/myshop/settings/base.py
+++ myshop/myshop/settings/base.py
@@ -155,21 +155,20 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 # ----------------------------------------------------------------------------
+# STRIPE SETTINGS
+# ----------------------------------------------------------------------------
+
+STRIPE_PUBLISHABLE_KEY = '' # Публикуемый ключ
+STRIPE_SECRET_KEY = '' # Секретный ключ
+STRIPE_API_VERSION = ''  # версия API на момент выпуска stripe 14.3.0
+STRIPE_WEBHOOK_SECRET = ''  # Секрет вебхука
+
+
+# ----------------------------------------------------------------------------
 # EMAIL
-# ----------------------------------------------------------------------------
-
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
-
-# ----------------------------------------------------------------------------
-# STRIPE
-# ----------------------------------------------------------------------------
-
-STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY")
-STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
-STRIPE_API_VERSION = os.environ.get("STRIPE_API_VERSION")
-STRIPE_WEBHOOK_SECRET = os.environ.get("STRIPE_WEBHOOK_SECRET")
-
+# -
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
